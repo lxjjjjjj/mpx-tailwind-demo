336b5435c1f54ff098e63542c8bef80a683071e1
sarahliu <sarahliu@didiglobal.com>
2022-04-14 14:26:19 +0800

    修改

--- mpx-project/tailwind.config.js
+++ mpx-project/tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   content: [],
+  purge: [
+    'dist/wx/home/**/**/*.wxml','dist/wx/sidebar/**/**/*.wxml','dist/wx/pages/*.wxml','dist/wx/components/**/*.wxml'
+  ],
   theme: {
     extend: {},
   },
